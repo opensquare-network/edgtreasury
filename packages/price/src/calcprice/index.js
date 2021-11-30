4f97c86fdc71e09245a8aac7efe4c077dea6dd1d
chaojun <chaojun@opensquare.network>
2021-11-30 16:30:06 +0800

    Refs #22, sync price to scan db

---
+++ packages/price/src/calcprice/index.js
@@ -0,0 +1,62 @@
+const dotenv = require("dotenv");
+dotenv.config();
+
+const DB = require("./output-scan");
+const { getPrice } = require("./price");
+
+const mongoUrl = process.env.MONGO_URL;
+if (!mongoUrl) {
+  console.log("MONGO_URL not set");
+  process.exit(1);
+}
+
+const outDbName = process.env.MONGO_DB_OUTPUT_DB_NAME;
+if (!outDbName) {
+  console.log("MONGO_DB_OUTPUT_DB_NAME not set");
+  process.exit(1);
+}
+
+const {
+  getTipCollection,
+  getProposalCollection,
+  getBountyCollection,
+} = DB(mongoUrl, outDbName);
+
+async function savePrice(col) {
+  const items = await col.find({}).toArray();
+
+  for (const item of items) {
+    if (item.symbolPrice !== undefined) {
+      continue;
+    }
+    const blockTime = item.indexer?.blockTime;
+    if (blockTime) {
+      const price = await getPrice(blockTime);
+      if (price) {
+        await col.updateOne(
+          { _id: item._id },
+          {
+            $set: {
+              symbolPrice: price,
+            },
+          }
+        );
+      }
+    }
+  }
+}
+
+async function main() {
+  const tipCol = await getTipCollection();
+  await savePrice(tipCol);
+
+  const proposalCol = await getProposalCollection();
+  await savePrice(proposalCol);
+
+  const bountyCol = await getBountyCollection();
+  await savePrice(bountyCol);
+
+  console.log("Update price successful:", dbName);
+}
+
+main().catch(console.error).then(() => process.exit(0));
