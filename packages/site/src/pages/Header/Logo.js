a8dc0d75c12c8daac1a01e2a9202ef4c3566480f
qiyisi <qiyisi@qq.com>
2021-12-03 11:34:46 +0800

    Add chain switch, #62

--- packages/site/src/pages/Header/Logo.js
+++ packages/site/src/pages/Header/Logo.js
@@ -10,14 +10,14 @@ const Wrapper = styled.div`
 
 const ImgFullWrapper = styled.div`
   display: block;
-  @media screen and (max-width: 390px) {
+  @media screen and (max-width: 500px) {
     display: none;
   }
 `;
 
 const ImgShortWrapper = styled.div`
   display: none;
-  @media screen and (max-width: 390px) {
+  @media screen and (max-width: 500px) {
     display: block;
   }
 `;
